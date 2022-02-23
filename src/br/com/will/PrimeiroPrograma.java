d01a09ef35921b0479ea7ef606477d541edd76c1
saogvs <saogvs@hotmail.com>
2022-02-23 14:39:56 -0300

    Primeiro Commit

---
+++ src/br/com/will/PrimeiroPrograma.java
@@ -0,0 +1,11 @@
+package br.com.will;
+//import br.com.will.Gato; não vai ser usado pq eu coloquei tudo na mesma pasta kkkk
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+        /*int a = 5;
+        int b = 6;
+        System.out.println("oi mundo " + (a+b));*/
+    }
+}
